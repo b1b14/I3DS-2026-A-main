22627010b010340560c05b85ec7f01124284c4ad
B1B14 <anabeatrizluiz201@gmail.com>
2026-05-15 09:15:19 -0300

    Add new asset image SV.jpg to project assets

--- I3DS-2026-A-main/07-DevSteam/src/components/Outros/Outros.jsx
+++ I3DS-2026-A-main/07-DevSteam/src/components/Outros/Outros.jsx
@@ -1,7 +1,33 @@
 import style from "./Outros.module.css";
 import CS from "../../assets/CS.jpg";
-
-const outrosJogos = [1, 2, 3, 4];
+import HK from "../../assets/HK.jpg";
+import SV from "../../assets/SV.jpg";
+import Cup from "../../assets/Cup.jpg";
+const outrosJogos = [ {
+    nome: "COUNTER STRIKE: GLOBAL OFFENSIVE",
+    imagem:CS ,
+    preco: "R$00,00",
+    descricao:  "Tático,FPS"
+  },
+  {
+    nome: "Hollow Knight",
+    imagem:HK ,
+    preco: "R$79,90",
+    descricao:  "Metroidvania,Ação, Aventura, Plataforma 2D",
+  },
+  {
+    nome: "Stardew Valley",
+    imagem:SV ,
+    preco: "R$69,99",
+    descricao:  "Simulação,Farming Simulator,RPG,Sandbox/vida cotidiana",
+  },
+  {
+    nome: "Cuphead",
+    imagem: Cup,
+    preco: "R$99,90",
+    descricao:  "Plataforma,Ação",
+  },
+];
 
 function Outros() {
   return (
@@ -10,12 +36,12 @@ function Outros() {
 
       {outrosJogos.map((item, index) => (
         <div className={style["jogo-horizontal"]} key={index}>
-          <img src={CS} alt="CS GO" />
+          <img src={item.imagem} alt={item.nome} />
 
           <div className={style["jogo-info"]}>
-            <h3>COUNTER STRIKE: GLOBAL OFFENSIVE</h3>
-            <p>Ação, Estratégia, Multiplayer.</p>
-            <span>R$00,00</span>
+            <h3>{item.nome}</h3>
+            <p>{item.descricao}</p>
+            <span>{item.preco}</span>
           </div>
 
           <button className={style.button}>ADICIONAR AO CARRINHO</button>
